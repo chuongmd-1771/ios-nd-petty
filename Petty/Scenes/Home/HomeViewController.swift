9e28d6dc5ca9605e08ad7f924719b80a4fb33277
mai.dinh.chuong <mai.dinh.chuong@sun-asterisk.com>
2019-04-05 09:54:25 +0700

    design_home_screen

---
+++ Petty/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Petty
+//
+//  Created by chuongmd on 4/5/19.
+//  Copyright © 2019 Sun-Asterisk. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
