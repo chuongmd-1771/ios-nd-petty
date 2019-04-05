11fe7e98ea26accec4cc4c1ffd47e7dcd7066242
mai.dinh.chuong <mai.dinh.chuong@sun-asterisk.com>
2019-04-05 12:54:52 +0700

    design_pet_detail_screen

---
+++ Petty/Scenes/Common/CustomView.swift
@@ -0,0 +1,42 @@
+//
+//  CustomView.swift
+//  Petty
+//
+//  Created by chuongmd on 4/5/19.
+//  Copyright © 2019 Sun-Asterisk. All rights reserved.
+//
+
+import UIKit
+
+final class CustomView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    private func setupView() {
+        clipsToBounds = true
+        backgroundColor = .white
+        layer.cornerRadius = 50
+        layer.shadowRadius = 2
+        dropShadow(color: .lightGray, opacity: 1, offSet: CGSize(width: 1, height: 1), radius: 20, scale: true)
+    }
+    
+    private func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offSet
+        layer.shadowRadius = radius
+        
+        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+}
---
+++ Petty/Scenes/Pet Detail/PetDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PetDetailViewController.swift
+//  Petty
+//
+//  Created by chuongmd on 4/5/19.
+//  Copyright © 2019 Sun-Asterisk. All rights reserved.
+//
+
+import UIKit
+
+final class PetDetailViewController: UIViewController {
+    @IBOutlet weak var petImageView: UIView!
+    @IBOutlet weak var petDetailView: UIView!
+    @IBOutlet weak var likeNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    @IBAction func likePressed(_ sender: Any) {
+    }
+    
+    fileprivate func setupUI() {
+        
+    }
+}
